ec714f880124e8401d20b842684de27ab14a5d1f
John Firebaugh <john.firebaugh@gmail.com>
2010-10-17 17:31:25 -0700

    Outline desired step definitions.

---
+++ lib/bermuda/cucumber.rb
@@ -0,0 +1,75 @@
+# Accordion
+
+Then /^I should see the following accordion sections:$/ do |titles|
+  pending
+end
+
+Then /^the "([^"]+)" accordion section should be active$/ do |title|
+  pending
+end
+
+Then /^the "([^"]+)" accordion section should not be active$/ do |title|
+  pending
+end
+
+When /^I activate the "([^"]+)" accordion section$/ do |title|
+  pending
+end
+
+# Autocomplete
+
+Then /^I should see the following completions:$/ do |choices|
+  pending
+end
+
+When /^I select the "([^"]+)" completion$/ do |title|
+  pending
+end
+
+# Dialog
+
+Then /^I should see the "([^"]+)" dialog$/ do |title|
+  pending
+end
+
+Then /^I should not see the "([^"]+)" dialog$/ do |title|
+  pending
+end
+
+When /^I close the "([^"]+)" dialog$/ do |title|
+  pending
+end
+
+# Progress bar
+
+Then /^the progress bar should be at (\d+)(?: percent|%)$/ do |value|
+  pending
+end
+
+# Slider
+
+Then /^the slider should be at (\d+)$/ do |value|
+  pending
+end
+
+When /^I set the slider to (\d+)$/ do |value|
+  pending
+end
+
+# Tab
+
+Then /^I should see the following tabs:$/ do |titles|
+  pending
+end
+
+Then /^the "([^"]+)" tab should be active$/ do |title|
+  pending
+end
+
+Then /^the "([^"]+)" tab should not be active$/ do |title|
+  pending
+end
+
+When /^I activate the "([^"]+)" tab$/ do |title|
+  pending
+end
